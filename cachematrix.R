1dcbe6338a79e42b43d73437a39f5a02d958ed02
Justin White <justinwhite15@gmail.com>
2014-08-23 20:07:23 -0700

    Coursera - R-Programming Course - Programming Assignment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Caching
+## These functions 
+## @date 8/23/14
+## @author github.com/justinwhite15
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## Function to create cache matrix object
+## @parameter   x   matrix to inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL        # Create variable to store cached inverse matrix
+  
+  # Create all of the functions of the object
+  # set - set the base matrix and null out any cached inverse matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  # Return the base matrix
+  get <- function() x
+  
+  # Store the inversed matrix
+  setInverseMatrix <- function(inverseMatrix) m <<- inverseMatrix
+  
+  # Get the inversed matrix
+  getInverseMatrix <- function() m
+  
+  #Return the functions as a list
+  list(
+        set = set, 
+        get = get,
+        setInverseMatrix = setInverseMatrix,
+        getInverseMatrix = getInverseMatrix
+  )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve
+## Function to return the inverse of the matrix x and cache the results
+## @parameter   x   makeCacheMatrix object
+cacheSolve <- function(x) {
+  # Get the inversed matrix from the cache matrix
+  m <- x$getInverseMatrix()
+  
+  # Cache-Hit - the cache matrix returns a non-null inversed matrix, so return it
+  if(!is.null(m)) {
+    return(m)
+  }
+  
+  # Cache-Miss - the cache matrix dosen't return an inversed matrix, so get the original matrix, calculate inverse, cache it, and return it
+  data <- x$get()
+  m <- solve(data)
+  x$setInverseMatrix(m)
+  m
 }
